f7fc05ccf71de21f550c97201ba2d6210fb0c80d
shoyonp <shoyonkumar143@gmail.com>
2024-09-19 20:56:04 +0600

    practise done

--- js/addMoney.js
+++ js/addMoney.js
@@ -2,6 +2,17 @@ document.getElementById('button-add-money').addEventListener('click',function(ev
     event.preventDefault();
 
     const addMoney = document.getElementById('input-add-money').value;
+    const addMoneyNumber = parseFloat(addMoney);
     const pinNumber = document.getElementById('input-pin-number').value;
-    console.log(addMoney,pinNumber);
+    if(pinNumber === '1234'){
+        const balance = document.getElementById('account-balance').innerText;
+        const balanceNumber = parseFloat(balance);
+
+        const newBalance = balanceNumber + addMoneyNumber;
+
+        document.getElementById('account-balance').innerText = newBalance;
+    }
+    else{
+        alert('Faild to add money.')
+    }
 })
